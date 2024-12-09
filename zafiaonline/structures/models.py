a040ff9147f0a37b2adadb075196dda6578b92b8
unelected <152802875+unelected@users.noreply.github.com>
2024-12-09 10:59:38 +0500

    Update models.py
    import skips fix

--- zafiaonline/structures/models.py
+++ zafiaonline/structures/models.py
@@ -1,8 +1,8 @@
 from msgspec import Struct
-from .packet_data_keys import Renaming
-from .enums import Sex, Languages, Roles
 from typing import List
 
+from .packet_data_keys import Renaming
+from .enums import Sex, Languages, Roles
 
 class ModelUser(Struct, rename=Renaming.USER):
     user_id: str = None
