37ab8e758ea832cfdd2fafc709e8f63b683d14c0
unelected <152802875+unelected@users.noreply.github.com>
2024-12-09 11:07:33 +0500

    Update __init__.py
    added absolute path

--- zafiaonline/structures/__init__.py
+++ zafiaonline/structures/__init__.py
@@ -1,6 +1,6 @@
-from .packet_data_keys import PacketDataKeys, Renaming
-from .enums import Sex, Roles, Languages, RatingMode, RatingType, ActivityType, RoomModelType
-from .models import (ModelUser, ModelServerConfig, ModelRoom,
+from zafiaonline.structures.packet_data_keys import PacketDataKeys, Renaming
+from zafiaonline.structures.enums import Sex, Roles, Languages, RatingMode, RatingType, ActivityType, RoomModelType
+from zafiaonline.structures.models import (ModelUser, ModelServerConfig, ModelRoom,
                      ModelShortUser, ModelFriend, ModelMessage)
 
 __all__ = (
