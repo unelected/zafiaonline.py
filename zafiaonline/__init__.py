9e09e06795d8f4f51de43b8c06f8377e3fac8e5b
unelected <152802875+unelected@users.noreply.github.com>
2024-12-09 11:12:02 +0500

    Update __init__.py
    absolute path

--- zafiaonline/__init__.py
+++ zafiaonline/__init__.py
@@ -1,6 +1,6 @@
-from .exceptions import ListenDataException, ListenExampleErrorException
-from .zafiaonline import Client
-from .web import WebClient
+from zafiaonline.exceptions import ListenDataException, ListenExampleErrorException
+from zafiaonline.zafiaonline import Client
+from zafiaonline.web import WebClient
 __all__ = (
     "Client",
     "WebClient",
