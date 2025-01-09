ff4a2975b9d8b6a79611d3a5342115df74bfa609
unelected <152802875+unelected@users.noreply.github.com>
2025-01-09 20:52:40 +0500

    Delete zafiaonline.py directory

--- zafiaonline.py/zafiaonline/__init__.py
+++
@@ -1,6 +0,0 @@
-from zafiaonline.zafiaonline import Client
-from zafiaonline.web import WebClient
-__all__ = (
-    "Client",
-    "WebClient"
-)
--- zafiaonline.py/zafiaonline/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from zafiaonline.utils.exceptions import ListenDataException, ListenExampleErrorException
-from zafiaonline.utils.md5hash import Md5
-
-__all__ = (
-    "Md5",
-    "ListenDataException",
-    "ListenExampleErrorException"
-)
