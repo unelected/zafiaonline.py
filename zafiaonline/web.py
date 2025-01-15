e62586abbd95262647a99ed4b5c8acd7fe0536bd
unelected <152802875+unelected@users.noreply.github.com>
2025-01-15 22:27:14 +0500

    Update web.py

--- zafiaonline/web.py
+++ zafiaonline/web.py
@@ -4,7 +4,7 @@ import base64
 from zafiaonline.structures.enums import Languages, Sex
 
 class WebClient:
-    def __init__(self, client, web_port) -> None:
+    def __init__(self, client) -> None:
         self.client = client
         self.web_port = web_port
         self.rest_address = f"http://{self.client.address}:{self.client.web_port}"
