cb2a8e4125af4708399fbde7ac4fe91cc714992f
unelected <152802875+unelected@users.noreply.github.com>
2025-01-29 22:12:27 +0500

    Update __init__.py

--- zafiaonline/__init__.py
+++ zafiaonline/__init__.py
@@ -1,4 +1,4 @@
-from zafiaonline.zafiaonline import Client
+from zafiaonline.main import Client
 from zafiaonline.web import WebClient
 from zafiaonline.websocket_module import Websocket
 from zafiaonline import utils, structures
