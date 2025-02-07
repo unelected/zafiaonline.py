0198b766691aae5109b1a951d6afe6513821ae05
unelected <152802875+unelected@users.noreply.github.com>
2025-02-07 08:52:56 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="zafiaonline",
     version="1.0",
-    packages=["zafiaonline"],
+    packages=["zafiaonline". "zafiaonline.*"],
     install_requires=["websockets", "msgspec"],
 )
