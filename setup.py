d2f7c85156f50d45bb8fe5e3c335141e1272e828
unelected <152802875+unelected@users.noreply.github.com>
2025-02-27 16:44:27 +0500

    Update 2.4.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zafiaonline",
-    version="2.4.11",
+    version="2.4.12",
     packages=find_packages(include=["zafiaonline", "zafiaonline.*"]),
     install_requires=["websockets", "msgspec", "sphinx"],
     description="A Python API library for interacting with Mafia Online game.",
