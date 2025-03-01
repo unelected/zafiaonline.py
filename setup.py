2bdb6fe88b2089d5b99c5477b19cd7f47f6e6494
unelected <152802875+unelected@users.noreply.github.com>
2025-03-02 02:55:24 +0500

    Update 2.5.1 fixed bug with disconnect on two script-connections

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zafiaonline",
-    version="2.5.0",
+    version="2.5.1",
     packages=find_packages(include=["zafiaonline", "zafiaonline.*"]),
     install_requires=["websockets", "msgspec", "sphinx"],
     description="A Python API library for interacting with Mafia Online game.",
