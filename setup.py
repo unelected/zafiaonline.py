570db245b9a0b80b53001a27ddb741e3f8d16b96
unelected <152802875+unelected@users.noreply.github.com>
2025-03-13 03:22:02 +0500

    Update 2.5.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zafiaonline",
-    version="2.5.2",
+    version="2.5.3",
     license='Pepsiware',
     packages=find_packages(include=["zafiaonline", "zafiaonline.*"]),
     install_requires=["websockets", "msgspec", "sphinx"],
