2ed1e4c482208b2a3e503bee3c0b7afc0ba3c1e1
unelected <152802875+unelected@users.noreply.github.com>
2025-03-20 04:38:33 +0500

    Update simple_bot.py

--- examples/simple_bot.py
+++ examples/simple_bot.py
@@ -1,58 +1,63 @@
 # simple mafia online bot
 import asyncio
+import logging
 
+from zafiaonline.utils import ApiDecorators
 from zafiaonline.utils.exceptions import ListenExampleErrorException
 from zafiaonline.main import Client
-from zafiaonline.structures import PacketDataKeys
 
-async def main():
-    user_agreement = input("[en] send messages may ban you, do you accept "
-                           "this? if "
-                      "don't "
-                "accept you'll use only chat listener\n"
-                "if you agree, write yes: \n\n"
-                "[ru] отправка сообщений может заблокировать аккаунт, "
-                           "принимаешь "
-                "ли ты это? если нет то будет доступна только прослушка чата\n"
-                "если ты согласен то введи да: ")
-    
+
+class Main:
+    def __init__(self):
+        self.user_agreement = None
+        self.user_id = None
+        self.user_name = None
+
+    async def main(self):
+        self.user_agreement = input("[en] send messages may ban you, do you "
+                                 "accept "
+                               "this? if "
+                          "don't "
+                    "accept you'll use only chat listener\n"
+                    "if you agree, write yes: \n\n"
+                    "[ru] отправка сообщений может заблокировать аккаунт, "
+                               "принимаешь "
+                    "ли ты это? если нет то будет доступна только прослушка чата\n"
+                    "если ты согласен то введи да: ")
+        print("\n\n\n")
+
+        await Mafia.sign_in("email", "password")
+
+        await Mafia.join_global_chat()  # join in global chat
+
+        while True:
+            try:
+                result = await Mafia.listen() # try listen data for result
+            except ListenExampleErrorException as e:
+                print("listen error", e)
+                continue
+
+            await self.message_handle(result)
+
+    @ApiDecorators.extract_message
+    async def message_handle(self, content):
+        print(f"[{self.user_name}]: {content}")  # print nickname
+        # with message
+        if self.user_id != Mafia.id:  # id sameness check
+            send_content = content  # in id with sender and you are
+            # not the same content will send
+        else:
+            send_content = None
+        if self.user_agreement == "yes" or self.user_agreement == "да":
+            if send_content:  # have content
+                await Mafia.send_message_global(
+                    send_content)  # send message to global chat
+
+
+if __name__ == "__main__":
     Mafia = Client()
-    await Mafia.sign_in("email", "password")
-
-    await Mafia.join_global_chat()  # join global chat
-
-    while True:
-        try:
-            result = await Mafia.listen() # try listen data for result
-        except ListenExampleErrorException as e:
-            print("listen error", e)
-            continue
-
-        if result[
-            PacketDataKeys.TYPE] == PacketDataKeys.MESSAGE:  # if new message
-            message = result[PacketDataKeys.MESSAGE]
-            message_type = message[PacketDataKeys.MESSAGE_TYPE]
-
-            if message_type == 1:  # if message type "text"
-                uu = message[PacketDataKeys.USER]  # message user info
-                content = message[PacketDataKeys.TEXT] # get text
-
-                user_id = uu[PacketDataKeys.OBJECT_ID] # get sender id
-                user_name = uu[PacketDataKeys.USERNAME] # get sender nickname
-
-                print(f"[{user_name}]: {content}") # print nickname
-                # with message
-
-                if user_id != Mafia.id: # id sameness check
-                    send_content = content # in id with sender and you are
-                    # not the same content will send
-                else:
-                    send_content = None
-
-                if user_agreement in ("yes", "да"):
-                    if send_content: # if have content
-                        await Mafia.send_message_global(
-                            send_content)  # send message to global chat
-
-logging.basicConfig(level=logging.INFO)
-asyncio.run(main())
+    main = Main()
+    try:
+        asyncio.run(main.main())
+    except KeyboardInterrupt:
+        logging.info("программа завершена выходом из программы")
