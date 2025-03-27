777498aaf18b56b1dc5e00457e2277dbf34f01da
unelected <152802875+unelected@users.noreply.github.com>
2025-03-27 05:02:41 +0500

    Update 3.0.0
    major update. added new fuctions and new logic

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zafiaonline",
-    version="2.6.1",
+    version="3.0.0",
     license='Pepsiware',
     packages=find_packages(include=["zafiaonline", "zafiaonline.*"]),
     install_requires=["websockets", "msgspec", "sphinx"],
