e24420d878347a6531c87307df5070a07caebde7
unelected <152802875+unelected@users.noreply.github.com>
2025-05-25 21:26:33 +0500

    Add files via upload

---
+++ tests/sipmle_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from zafiaonline import Client
+from zafiaonline.structures.packet_data_keys import PacketDataKeys
+
+class TestClient(unittest.IsolatedAsyncioTestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user_id = "user_57e6cce718056"
+        self.nickname = "wow1one"
+
+    async def test_get_user(self):
+        await self.client.create_connection()
+        get_user = await self.client.get_user(self.user_id)
+        self.assertIn(self.user_id, get_user[PacketDataKeys.USER][
+            PacketDataKeys.OBJECT_ID])
+
+if __name__ == "__main__":
+    unittest.main()
