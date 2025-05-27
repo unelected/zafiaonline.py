893f707f9341ace9435eeafdc5e532a8cfa36735
unelected <152802875+unelected@users.noreply.github.com>
2025-05-27 17:58:35 +0500

    Add files via upload

--- zafiaonline/__init__.py
+++ zafiaonline/__init__.py
@@ -1,16 +1,16 @@
 import zafiaonline.utils as utils
 import zafiaonline.structures as structures
 import zafiaonline.api_client as api_client
+import zafiaonline.transport as transport
 
 from zafiaonline.main import Client
-from zafiaonline.websocket_module import Websocket
 
 __all__ = (
     # Classes
     "Client",
-    "Websocket",
 
     # Directories
+    "transport",
     "utils",
     "structures",
     "api_client",
