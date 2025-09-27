eb5dbeccff456d9be9bfe558804fc2f807ec3dc7
unelected <152802875+unelected@users.noreply.github.com>
2025-09-28 00:33:58 +0500

    9.0.1
    Improvements and bug fixes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath("../../src"))
 project = 'zafiaonline'
 copyright = '2025, unelected'
 author = 'unelected'
-release = '9.0.0'
+release = '9.0.1'
 
 autodoc_member_order = "bysource"
 autodoc_typehints = "description"
